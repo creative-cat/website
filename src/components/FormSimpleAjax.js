6cb4f65242edc9f0385cafa2e726dc6e6e724468
creative-cat <11723811+creative-cat@users.noreply.github.com>
2019-03-15 06:04:00 -0600

    Update FormSimpleAjax.js

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -10,7 +10,7 @@ class Form extends React.Component {
     name: 'Simple Form Ajax',
     subject: '', // optional subject of the notification email
     action: '',
-    successMessage: 'Thanks for your inquiry, we will get back to you soon',
+    successMessage: 'There is a problem, your message has not been sent, please try contacting us via email',
     errorMessage:
       'There is a problem, your message has not been sent, please try contacting us via email'
   }
